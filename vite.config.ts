e3ad0cd1b31412a1fb5dd3c3b5c3fa388cfc40f1
Dmitriy Novikov <dmatazev@gmail.com>
2026-03-09 19:00:09 +0300

    rebuild site

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/nexus-blade/',
-  server: {
-    host: true,
-    port: 5173
-  },
-  build: {
-    sourcemap: false,
-    target: 'es2020'
-  }
-});
+  base: '/nexus-blade/'
+})
